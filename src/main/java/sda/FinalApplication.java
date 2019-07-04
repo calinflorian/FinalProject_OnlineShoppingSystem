5f3c67452e7685d3cd6b4fa76086ee0d11431dd7
George Grigorescu <george.sandu.grigorescu@gmail.com>
2019-07-04 21:58:52 +0300

    initial commit of the final sda project template

---
+++ src/main/java/sda/FinalApplication.java
@@ -0,0 +1,16 @@
+package sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalApplication {
+
+    //TODO Change the name of the project/this class from FinalApplication to the name of your own
+    // project.
+    // This should be changed in the POM file as well. See line 12.
+    public static void main(String[] args) {
+        SpringApplication.run(FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sda/FinalApplicationTests.java
@@ -0,0 +1,16 @@
+package sda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
